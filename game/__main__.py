f2c04d09aa90acf407ef987ae439aa75608fb403
AlbertLEngedal <code@engedal.biz>
2025-10-08 09:14:54 +0200

    Starting structure

--- game/__main__.py
+++ game/__main__.py
@@ -4,19 +4,51 @@ import time
 
 import pygame as pg
 
-from game.player import Player
+from player import Player
+
+class ScrollingBG:
+    def __init__(self, image_path: str, speed: tuple[int, int], screen_size: tuple[int, int]):
+        self.img = pg.image.load(image_path).convert()
+        self.w, self.h = self.img.get_size()
+        self.speed_x, self.speed_y = speed
+        
+        # self.img = pg.image.load(image_path).convert()
+        self.img = pg.transform.smoothscale_by(self.img, 1)
+        
+        self.off_x = 0
+        self.off_y = 0
+        self.screen_w, self.screen_h = screen_size
+
+    def update(self, dt: float):
+        self.off_x = (self.off_x + self.speed_x * dt) % self.w
+        self.off_y = (self.off_y + self.speed_y * dt) % self.h
+
+    def draw(self, surf: pg.Surface):
+        ox, oy = int(self.off_x), int(self.off_y)
+        # top-left tile start
+        start_x = -ox
+        start_y = -oy 
+        # draw just enough tiles to cover screen (at most 4)
+        for y in (start_y, start_y + self.h):
+            for x in (start_x, start_x + self.w):
+                surf.blit(self.img, (x, y))
+
+
 
 
 pg.init()
 
-def get_scaled_resolution(scale: float) -> tuple[int, int]:
-    base_width, base_height = 1920, 1080
-    width = int(base_width * scale)
-    height = int(base_height * scale)
-    return width, height
+# def get_scaled_resolution(scale: float) -> tuple[int, int]:
+#     base_width, base_height = 1920, 1080
+#     width = int(base_width * scale)
+#     height = int(base_height * scale)
+#     return width, height
 
 # Screen
-WIDTH, HEIGHT = get_scaled_resolution(1)
+# WIDTH, HEIGHT = get_scaled_resolution(1)
+WIDTH = 1920
+HEIGHT = 1080
+
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Fighter Jet Game")
 
@@ -29,7 +61,15 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
-Player1 = Player(200, 200)  
+Player1 = Player(300, 300)  
+
+# init
+# screen_size = (screen.get_width, screen.get_height)
+screen_size = (1920, 1080)
+bg = ScrollingBG("assets/background/bg.png", speed=( +80, 0 ), screen_size=screen_size)
+
+
+
 
 # --- Game Loop ---
 running = True
@@ -39,12 +79,15 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-
+    # main loop
+    dt = clock.get_time() / 1000.0
+    bg.update(dt)
+    bg.draw(screen)
   
 
     Player1.handle_keys()
     
-    screen.fill(BLACK)
+    # screen.fill(BLACK)
     Player1.draw(screen)
     
 
@@ -68,3 +111,5 @@ while running:
 
 pg.quit()
 sys.exit()
+
+
--- game/player.py
+++ game/player.py
@@ -7,10 +7,26 @@ def scale_to_width(image, target_w: int):
 
 class Player:
     def __init__(self, x, y):
-        self.image = pg.image.load("assets/jetthing.png").convert_alpha()
-        self.image = scale_to_width(self.image,100)
+        self.image = pg.image.load("assets/jet/afterburner0.png").convert_alpha()
+        self.image = scale_to_width(self.image,200)
+        self.image = pg.transform.flip(self.image, True, False)
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = 5
+        self.x = x
+        self.y = y
+        
+    def new_image(self, variation):
+        if variation == 0:
+            self.image = pg.image.load("assets/jet/afterburner0.png").convert_alpha()
+            # self.speed = 5
+        if variation == 1:
+            self.image = pg.image.load("assets/jet/afterburner3.png").convert_alpha()
+            # self.speed = 5
+            
+        self.image = scale_to_width(self.image,200)
+        self.image = pg.transform.flip(self.image, True, False)
+        # self.rect = self.image.get_rect(center=(self.x, self.y))
+            
 
     def handle_keys(self):
         keys = pg.key.get_pressed()
@@ -18,6 +34,10 @@ class Player:
             self.rect.x -= self.speed
         if keys[pg.K_RIGHT]:
             self.rect.x += self.speed
+            self.new_image(1)
+        else:
+            self.new_image(0)
+            
         if keys[pg.K_UP]:
             self.rect.y -= self.speed
         if keys[pg.K_DOWN]:
